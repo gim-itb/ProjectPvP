628190800d5e603b48056796d0ba965b79d707f0
DeltDev <deltenc88@gmail.com>
2023-06-09 08:22:20 +0700

    animasi pintu

--- Assets/Mechanic Building/Items/Code/KeyScript.cs
+++ Assets/Mechanic Building/Items/Code/KeyScript.cs
@@ -5,20 +5,50 @@ using UnityEngine;
 public class KeyScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    
-    public GameObject door;//dapetin object door
 
+    #region Attributes
+    public float OpenSpeed = 5f; //kecepatan buka pintu
+    public float fadeSpeed = 10f; //kecepatan koin hilang
+    public Transform door;
+    public Vector2 StartPos = new Vector3(10.407f, -0.86f,0); //posisi awal pintu
+    public Vector2 EndPos = new Vector3(10.407f, 3.55f,0); //posisi akhir pintu
+    private bool open = false; //flag pintu
+    SpriteRenderer keySprite; //dapetin sprite pintu
+    #endregion
+
+    #region Start
+    private void Start()
+    {
+        keySprite = GetComponent<SpriteRenderer>();
+    }
+    #endregion
+
+    #region "Animations"
+    private void Update()
+    {
+        if (open)
+        {
+            door.position = Vector3.Lerp(door.position, EndPos, OpenSpeed * Time.deltaTime); //animasi pintu gerak ke atas
+            keySprite.color = Color.Lerp(keySprite.color, new Color(keySprite.color.r,keySprite.color.g,keySprite.color.b,0),fadeSpeed*Time.deltaTime); //animasi fade trigger
+        }
+    }
+    #endregion
+
+    #region Trigger Script
     //Trigger kunci biar bisa buka pintu
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.CompareTag("Player"))
+        if (col.tag == "Player") //jika tabrakan sama objek yang tagnya player
         {
-            Debug.Log("kunci diambil");
+            //Debug.Log("kunci diambil");
             //pintu hilang instan
-            door.SetActive(false);
+            //door.SetActive(false);
+            open = true; //tandai flag open jadi true
             //pintu animasi gerak ke atas
 
-            this.gameObject.SetActive(false);
+            //matikan trigger
+            //this.gameObject.SetActive(false);
         }
     }
+    #endregion
 }
